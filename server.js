6c71df8d86c68da387dcd51b55cb3a5fa909238e
Kyle Macquarrie <kyle@kylemacquarrie.co.uk>
2014-01-29 15:22:56 +0000

    change messages

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ wss.on('connection', function (ws) {
         var decoded = JSON.parse(data);
         console.log('tiltLR: ' + decoded.tiltLR + ' tiltFB: ' + decoded.tiltFB + ' dir: ' + decoded.dir);
         //console.log(message);
-        ws.send('back at you');
+        ws.send(data);
     });
     
     

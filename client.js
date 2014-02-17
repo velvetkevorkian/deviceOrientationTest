efa264e16114003d50f567751d0271bb627cff12
Kyle Macquarrie <kyle@velvetkevorkian.co.uk>
2014-02-17 14:45:50 +0000

    remove motion handlers for now

--- client.js
+++ client.js
@@ -5,17 +5,17 @@ var msg = {
     tiltFB: 0,
     dir: 0,
 
-    accelX: 0,
-    accelY: 0,
-    accelZ: 0,
-
-    gravAccelX: 0,
-    gravAccelY: 0,
-    gravAccelZ: 0,
-
-    rotationRateX: 0,
-    rotationRateY: 0,
-    rotationRateZ: 0
+//    accelX: 0,
+//    accelY: 0,
+//    accelZ: 0,
+//
+//    gravAccelX: 0,
+//    gravAccelY: 0,
+//    gravAccelZ: 0,
+//
+//    rotationRateX: 0,
+//    rotationRateY: 0,
+//    rotationRateZ: 0
 };
 
 ws.onopen = function () {
@@ -39,11 +39,11 @@ function init() {
         document.getElementById("doEvent").innerHTML = "Not supported on your device or browser.  Sorry."
     }
 
-    if ((window.DeviceMotionEvent) || ('listenForDeviceMovement' in window)) {
-        window.addEventListener('devicemotion', deviceMotionHandler, false);
-    } else {
-        document.getElementById("dmEvent").innerHTML = "Not supported on your device or browser.  Sorry."
-    }
+//    if ((window.DeviceMotionEvent) || ('listenForDeviceMovement' in window)) {
+//        window.addEventListener('devicemotion', deviceMotionHandler, false);
+//    } else {
+//        document.getElementById("dmEvent").innerHTML = "Not supported on your device or browser.  Sorry."
+//    }
 
 
 
@@ -59,48 +59,50 @@ function deviceOrientationHandler(tiltLR, tiltFB, dir) {
     ws.send(JSON.stringify(msg));
 }
 
-function deviceMotionHandler(eventData) {
-    var info, xyz = "[X, Y, Z]";
-
-    var acceleration = eventData.acceleration;
-    info = xyz.replace("X", round(acceleration.x));
-    info = info.replace("Y", round(acceleration.y));
-    info = info.replace("Z", round(acceleration.z));
-    document.getElementById("moAccel").innerHTML = info;
-
-    var accelerationWithGravity = eventData.accelerationIncludingGravity;
-    info = xyz.replace("X", round(accelerationWithGravity.x));
-    info = info.replace("Y", round(accelerationWithGravity.y));
-    info = info.replace("Z", round(accelerationWithGravity.z));
-    document.getElementById("moAccelGrav").innerHTML = info;
-
-    var rotation = eventData.rotationRate;
-    info = xyz.replace("X", round(rotation.alpha));
-    info = info.replace("Y", round(rotation.beta));
-    info = info.replace("Z", round(rotation.gamma));
-    document.getElementById("moRotation").innerHTML = info;
-
-    info = eventData.interval;
-    document.getElementById("moInterval").innerHTML = info;
-
-    msg.accelX = acceleration.x;
-    msg.accelY = acceleration.y;
-    msg.accelZ = acceleration.z;
-
-    msg.gravAccelX = accelerationWithGravity.x;
-    msg.gravAccelY = accelerationWithGravity.y;
-    msg.gravAccelZ = accelerationWithGravity.z;
-
-    msg.rotationRateX = rotation.alpha;
-    msg.rotationRateY = rotation.beta;
-    msg.rotationRateZ = rotation.gamma;
-
-
-}
+//function deviceMotionHandler(eventData) {
+//    var info, xyz = "[X, Y, Z]";
+//
+//    var acceleration = eventData.acceleration;
+//    info = xyz.replace("X", round(acceleration.x));
+//    info = info.replace("Y", round(acceleration.y));
+//    info = info.replace("Z", round(acceleration.z));
+//    document.getElementById("moAccel").innerHTML = info;
+//
+//    var accelerationWithGravity = eventData.accelerationIncludingGravity;
+//    info = xyz.replace("X", round(accelerationWithGravity.x));
+//    info = info.replace("Y", round(accelerationWithGravity.y));
+//    info = info.replace("Z", round(accelerationWithGravity.z));
+//    document.getElementById("moAccelGrav").innerHTML = info;
+//
+//    var rotation = eventData.rotationRate;
+//    info = xyz.replace("X", round(rotation.alpha));
+//    info = info.replace("Y", round(rotation.beta));
+//    info = info.replace("Z", round(rotation.gamma));
+//    document.getElementById("moRotation").innerHTML = info;
+//
+//    info = eventData.interval;
+//    document.getElementById("moInterval").innerHTML = info;
+//
+//    msg.accelX = acceleration.x;
+//    msg.accelY = acceleration.y;
+//    msg.accelZ = acceleration.z;
+//
+//    msg.gravAccelX = accelerationWithGravity.x;
+//    msg.gravAccelY = accelerationWithGravity.y;
+//    msg.gravAccelZ = accelerationWithGravity.z;
+//
+//    msg.rotationRateX = rotation.alpha;
+//    msg.rotationRateY = rotation.beta;
+//    msg.rotationRateZ = rotation.gamma;
+//
+//
+//}
 
 function round(val) {
     var amt = 10;
     return Math.round(val * amt) / amt;
 }
 
+
+//checkSupport();
 init();
